f197c9bd992df922366132fa851b16cd3e6e5fa4
ramgopalkasireddy <63334453+ramgopalkasireddy@users.noreply.github.com>
2023-03-23 04:05:09 +0530

    Create parameters.sv

---
+++ parameters.sv
@@ -0,0 +1,16 @@
+`ifndef PARAMETER_H_
+`define PARAMETER_H_
+
+// Parameter file
+
+`define instruction_column 16 
+`define data_column 16 
+`define instruction_row 15
+`define data_row 8 
+`define filename "output.txt"
+`define simulation_time #160
+
+//this is to enable random input instruction
+`define RANDOM_INSTRUCTION_INPUT
+
+`endif
